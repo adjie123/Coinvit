0d450a2e5e920a81893e67d988ed50f46638d7d1
adjie123 <adjiedwi6@gmail.com>
2018-08-30 15:34:22 +0700

    layouting

---
+++ confirm.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Coinvit</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition" style="background-color: #f7f7f7;">
+<div class="register-box">
+  <div class="register-logo">
+    <a href="../../index2.html">Hello, friends</a>
+  </div>
+
+  <div class="callout callout-success">
+    <h4>Yeah, your account has been active.</h4>
+
+    <p>Please sign in to complete your profile.</p>
+  </div>
+
+  <div class="register-box-body">
+   <form action="../../index.html" method="post">
+      <div class="form-group has-feedback">
+        <label>Email address</label>
+        <input type="text" class="form-control" placeholder="Your email address">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <label>Password</label>
+        <input type="password" class="form-control" placeholder="Password">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="checkbox">
+            <label>
+              <input type="checkbox">Remember me 
+            </label>
+            <label style="float: right;">
+              <a href="#" class="text-right"> Forgot password ?</a>
+            </label>
+          </div>
+        </div>
+        <!-- /.col -->
+
+        <div class="col-xs-12">
+          <button type="submit" class="btn btn-default btn-block" style="padding: 8px; margin-bottom: 10px; margin-top: 8px; font-size: 17px;">Login</button>
+          <span class="text-center">Not yet an account?<a href="register.php" class="text-center"> Register</a></span>
+        </div>
+        <!-- /.col -->
+        
+      </div>
+    </form>
+
+
+  </div>
+  <!-- /.form-box -->
+</div>
+<!-- /.register-box -->
+
+<!-- jQuery 3 -->
+<script src="../../bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
