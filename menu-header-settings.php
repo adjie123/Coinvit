b38efa59a417ab596d0b1ea73a278a3ebee5591d
adjie123 <adjiedwi6@gmail.com>
2018-12-11 08:10:00 +0700

    Setting UI

---
+++ menu-header-settings.php
@@ -0,0 +1,70 @@
+<header class="main-header">
+      <nav>
+        <!-- Navbar Right Menu -->
+        <div class="collapse navbar-collapse" style="background-color: #000;">
+
+          <ul class="nav navbar-nav navbar-right">
+           
+            <li><a href="balance.php" class="text-white">Markets</a></li>
+
+            <li class="dropdown">
+              <a class="dropdown-toggle text-white" data-toggle="dropdown" href="#">
+               Decentralize <span class="caret"></span>
+              </a>
+
+              <ul class="dropdown-menu">
+                <li><a tabindex="-1" href="#">Generate new wallet</a></li>
+                <li><a tabindex="-1" href="#">Login with your own private key</a></li>
+              </ul>
+
+            </li>
+            
+
+            <li class="dropdown">
+                <a class="dropdown-toggle text-white" data-toggle="dropdown" href="#">
+                  Centralize <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu">
+                  <li class="dropdown-submenu">
+                    <a class="submenu" tabindex="-1" href="#">
+                      Coinvit Deposit Withdrawal <span class="caret"></span>
+                    </a>
+
+                    <ul class="dropdown-menu">
+                      <li><a tabindex="-1" href="#">BUY CRYPTO with CREDIT CARD</a></li>
+                    </ul>
+
+                  </li>
+
+                </ul>
+            </li>
+            
+            
+            <li>
+              <a href="#" class="text-white">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/d/db/Google_Translate_Icon.png" width="20" height="20">    
+              </a>
+            </li>
+            
+            <li>
+              <a href="#" class="text-white">
+                <i class="fa fa-moon-o"></i>    
+              </a>
+            </li>
+            
+            <li>
+              <a href="#" data-toggle="modal" data-target="#modal-lr" class="text-white">
+                  Login / Register 
+              </a>
+            </li>
+            
+            <li>
+              <a href="#">
+                <i class="fa fa-wifi text-green"></i>    
+              </a>
+            </li>
+          </ul>
+        
+        </nav>
+      </header>
+      <!-- Left side column. contains the logo and sidebar -->
---
+++ menu-sidebar-settings.php
@@ -0,0 +1,32 @@
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+    
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu" style="margin: 8px; color: white;">
+      	<span style="color: grey; font-size: 20px;">SUMMARY</span>
+        <li class="active treeview"  style="font-size: 15px; margin-left: 10px; margin: 8px;">
+          <a href="myactivity.php">
+            <span>My Activity</span>
+          </a>
+        </li>
+
+        <span style="color: grey; font-size: 20px;">ACCOUNT</span>
+        <li class="treeview"  style="font-size: 15px; margin-left: 10px; margin: 8px;">
+          <a href="myprofile.php">
+            <span>My Profile</span>
+          </a>
+        </li>
+        <li class="treeview"  style="font-size: 15px; margin-left: 10px; margin: 8px;">
+          <a href="idverif.php">
+            <span>Identity Verification</span>
+          </a>
+        </li>
+        <li class="treeview"  style="font-size: 15px; margin-left: 10px; margin: 8px;">
+          <a href="#">
+            <span>Enable Account</span>
+          </a>
+        </li>
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
